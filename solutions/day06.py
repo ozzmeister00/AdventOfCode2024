2eab70dd383af543eabe30ceab87329155ebf750
Matt Oztalay <ozzmeister00@gmail.com>
2024-12-09 18:47:08 -0500

    Day 6 in the can

--- solutions/day06.py
+++ solutions/day06.py
@@ -31,11 +31,57 @@ class Guard(object):
         return Guard.FACING[self.direction]
 
 
+class GridSquare(list):
+    def __init__(self, position: utils.math.Int2):
+        super(GridSquare, self).__init__()
+        self.position = position
+
+    def canVisit(self) -> bool:
+        return True
+
+    def visit(self, direction: utils.math.Int2) -> bool:
+        """
+
+        :param direction:
+        :return: if this square has been visited from the input direction previously
+        """
+        if direction in self:
+            return True
+
+        self.append(direction)
+
+        return False
+
+    def hasBeenVisited(self) -> bool:
+        return len(self) > 0
+
+    def __str__(self):
+        if len(self) == 0:
+            return '.'
+        elif len(self) == 1:
+            if self[0].x != 0:
+                return '-'
+            if self[0].y != 0:
+                return '|'
+        else:
+            return '+'
+
+
+class Obstruction(GridSquare):
+    def canVisit(self) -> bool:
+        return False
+
+    def __str__(self):
+        return OBSTRUCTION
+
+
 class Solver(solver.solver.ProblemSolver):
     def __init__(self, rawData=None):
         # initialize values we'll access later
         self.grid = None
         self.guard = None
+        self.visitedPositions = []
+        self.initialGuardPosition = None
 
         super(Solver, self).__init__(6, rawData=rawData)
 
@@ -48,6 +94,7 @@ class Solver(solver.solver.ProblemSolver):
         rawData = self.rawData.replace('\n', '')
 
         self.grid = utils.math.Grid2D(width, data=rawData)
+
         indexes = self.grid.findIndexes(INITIAL_GUARD_CHARACTER)
         assert(len(indexes) == 1)
 
@@ -55,22 +102,30 @@ class Solver(solver.solver.ProblemSolver):
         # "South" in my coordinate space is "Up" if you're looking at the input data
         # TODO 2024_12_08 one day I'll have to transpose all this stuff so that it's drawn properly
         # TODO but interpreted without me having to do a bunch of mental gymnastics
-        self.guard = Guard(self.grid.indexToCoords(indexes[0]), self.grid.South)
+        self.initialGuardPosition = self.grid.indexToCoords(indexes[0])
+        self.guard = Guard(self.initialGuardPosition, self.grid.South)
 
-    def plotGuardsPath(self):
+        # once the grid is populated with its characters, loop back through and replace it all with GridSquares
+        for coord, character in self.grid.enumerateCoords():
+            if character == OBSTRUCTION:
+                self.grid[coord] = Obstruction(coord)
+            else:
+                self.grid[coord] = GridSquare(coord)
+
+    def plotGuardsPath(self) -> bool:
         """
         Resolve the guard's path until they leave the area
 
         If there is something direcly in front of you, turn right 90 degrees
         otherwise, keep walking
+
+        :return: if an infinite loop was detected in this path
         """
         directionIndex = 0
         clockwise = [self.grid.South, self.grid.Right, self.grid.Up, self.grid.Left]
 
         # while the guard's position is in bounds of the grid
         while self.grid.coordsInBounds(self.guard.position):
-            self.grid[self.guard.position] = VISITED
-
             # brute force would have us advance the guard's position once each cycle
             # but we're evolved creatures, and that's just silly, so instead
             # grab the line from in front of the guard to the edge of the map
@@ -89,14 +144,23 @@ class Solver(solver.solver.ProblemSolver):
             elif self.guard.direction.y > 0:
                 end.y = self.grid.height - 1
 
+            originalDirection = self.guard.direction
+
             path = utils.math.Line2D(start, end)
             pathItems = self.grid[path]
-            if OBSTRUCTION in pathItems:
-                # figure out where the first obstruction is
-                firstObstruction = pathItems.index(OBSTRUCTION) - 1
 
+            firstObstruction = 0
+            foundHit = False
+            while firstObstruction < len(pathItems) and not foundHit:
+                if not pathItems[firstObstruction].canVisit():
+                    foundHit = True
+                else:
+                    firstObstruction += 1
+
+            # if we found a hit
+            if foundHit:
                 # then move the end point there, less one
-                end = start + (self.guard.direction * firstObstruction)
+                end = start + (self.guard.direction * (firstObstruction - 1))
 
                 path = utils.math.Line2D(start, end)
 
@@ -106,13 +170,18 @@ class Solver(solver.solver.ProblemSolver):
 
                 self.guard.position = end
                 self.guard.direction = clockwise[directionIndex]
+
             # if we don't find any obstructions, then we can move the guard out of bounds
             else:
                 self.guard.position = end + self.guard.direction
 
-            # finally, mark all the points in the path as visited
+            # finally, mark all the points in the path as visited in the original direction
             for coord in path.getIntegerPoints():
-                self.grid[coord] = VISITED
+                # if this position has been visited previously from this direction, we can assume that we're now in a loop
+                if self.grid[coord].visit(originalDirection):
+                    return True
+
+        return False
 
     def SolvePartOne(self) -> int:
         """
@@ -121,13 +190,20 @@ class Solver(solver.solver.ProblemSolver):
         # plot the guard's path using the known rules
         self.plotGuardsPath()
 
+        # we'll reuse this in part 2
+        self.visitedPositions = [cell.position for cell in self.grid if cell.hasBeenVisited()]
+
         # then get the number of visited locations
-        return self.grid.count(VISITED)
+        return len(self.visitedPositions)
 
     def SolvePartTwo(self) -> int:
         """
         :return int: the number of positions that could contain an obstruction which would cause the guard's path to loop
         """
+        # if part one hasn't been run, then we need to do an initial pass at plotting the guard's path
+        if not self.visitedPositions:
+            self.SolvePartOne()
+
         result = 0
 
         # maybe we ARE merely mortal, and brute forcing is the only true path?
@@ -139,6 +215,17 @@ class Solver(solver.solver.ProblemSolver):
         # 1. Have a way to detect if the guard ends up in a loop
         # 2. Place on obstruction on all the places the guard visits with the exception of the starting position
         #    and re-run the simulation, counting all the variants in which the guard ends up in an infinite loop
+
+        for position in self.visitedPositions:
+            if position != self.initialGuardPosition:
+                # reprocess the inputs to reset the grid and guard
+                self.ProcessInput()
+
+                # then place ONE obstruction at this visited position
+                self.grid[position] = Obstruction(position)
+
+                if self.plotGuardsPath():
+                    result += 1
 
         return result
 
