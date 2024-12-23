1c739aaad66422a85cb61795fd8109f803b18282
Matt Oztalay <ozzmeister00@gmail.com>
2024-12-23 10:02:59 -0600

    Day 9 part 2

--- solutions/day09.py
+++ solutions/day09.py
@@ -1,5 +1,31 @@
 """
+Upon completion, two things immediately become clear. First, the disk definitely has a lot 
+more contiguous free space, just like the amphipod hoped. Second, the computer is running 
+much more slowly! Maybe introducing all of that file system fragmentation was a bad idea?
+
+The eager amphipod already has a new plan: rather than move individual blocks, he'd like
+ to try compacting the files on his disk by moving whole files instead.
+
+This time, attempt to move whole files to the leftmost span of free space blocks that could
+ fit the file. Attempt to move each file exactly once in order of decreasing file ID number
+  starting with the file with the highest file ID number. If there is no span of free space 
+  to the left of a file that is large enough to fit the file, the file does not move.
+
+The first example from above now proceeds differently:
+
+00...111...2...333.44.5555.6666.777.888899
+0099.111...2...333.44.5555.6666.777.8888..
+0099.1117772...333.44.5555.6666.....8888..
+0099.111777244.333....5555.6666.....8888..
+00992111777.44.333....5555.6666.....8888..
+The process of updating the filesystem checksum is the same; now, this example's checksum would be 2858.
+
+Start over, now compacting the amphipod's hard drive using this new method instead. 
+What is the resulting filesystem checksum?
+
+
 """
+import copy
 import itertools
 import sys
 
@@ -18,35 +44,80 @@ class File(object):
         self.size = size
         self.freeSpace = freeSpace
 
+    def __eq__(self, other) -> bool:
+        if isinstance(other, File):
+            return self.id == other.id and self.size == other.size
+        else:
+            raise ValueError(f"Cannot compare type {str(type(other))} with File")
+
+
+class FileSystem(list):
+    def __init__(self, rawData: str):
+        super(FileSystem, self).__init__()
+
+        mapped = list(map(int, rawData))
+        index = 0
+        for batch in list(itertools.batched(mapped, 2)):
+            blockSize = batch[0]
+            freeSpace = batch[1] if len(batch) > 1 else 0
+            self.append(File(index, blockSize, freeSpace))
+            index += 1
+
+    def expandToBlocks(self) -> list[int]:
+        """
+        Return an expanded version of the harddrive as individual blocks
+        where each item in the returned list is equal to the id of the 
+        file that occupies that block. If the space is free, the value 
+        is -1
+        """
+        harddrive = []
+        for fileInfo in self:
+            harddrive.extend([fileInfo.id] * fileInfo.size)
+            harddrive.extend([-1] * fileInfo.freeSpace)
+
+        return harddrive
+
+    def checksum(self) -> int:
+        """
+        Generate the checksum of the harddrive by 
+        multiplying the file ID by its position in the harddrive
+        """
+        harddrive = self.expandToBlocks()
+        result = 0
+        for index, blockID in enumerate(harddrive):
+            if blockID > -1:
+                result += (index * blockID)
+
+        return result
+
+    def __str__(self) -> str:
+        harddrive = self.expandToBlocks()
+
+        fileSystemStr = ''
+
+        for i, value in enumerate(harddrive):
+            fileSystemStr += '.' if value == -1 else str(value)
+
+        return fileSystemStr
+
 
 class Solver(solver.solver.ProblemSolver):
     def __init__(self, rawData=None):
         super(Solver, self).__init__(9, rawData=rawData)
 
-    def ProcessInput(self) -> list[File]:
+    def ProcessInput(self) -> FileSystem:
         """
-        :returns list[File]
+        :return: raw data translated into a FileSystem
         """
-        mapped = list(map(int, self.rawData))
-        fileList = []
-        index = 0
-        for batch in list(itertools.batched(mapped, 2)):
-            blockSize = batch[0]
-            freeSpace = batch[1] if len(batch) > 1 else 0
-            fileList.append(File(index, blockSize, freeSpace))
-            index += 1
+        return FileSystem(self.rawData)
 
-        return fileList
 
     def SolvePartOne(self) -> int:
         """
         :return int: the checksum of the filesystem
         """
         # build out the full map of the harddrive
-        harddrive = []
-        for fileInfo in self.processed:
-            harddrive.extend([fileInfo.id] * fileInfo.size)
-            harddrive.extend([-1] * fileInfo.freeSpace)
+        harddrive = self.processed.expandToBlocks()
 
         # defrag the drive by moving the right-most occupied block to the left-most available free space
         # until all free space is on the right
@@ -68,32 +139,47 @@ class Solver(solver.solver.ProblemSolver):
         :return int: the checksum of the partially defragged filesystem
         """
         # make a copy of the harddrive in case something goes wrong
-        fileSystem = self.processed.copy()
+        fileSystem = copy.copy(self.processed)
 
         # in decreasing fileID number, attempt to move the whole file to the leftmost available free space
         # if there is not a free space available, leave the file in place!
 
+        # search order is reversed of the file system
         searchOrder = fileSystem[::-1]
 
         # TODO don't try and do this in place, just build a new one?
 
-        #for fileInfo in searchOrder:
-        #    for i, searchFileInfo in enumerate(fileSystem[:fileSystem.index(fileInfo)]):
-        #        if searchFileInfo.freeSpace <= fileInfo.freeSpace:
-        #            fileSystem.pop(i)
+        for fileInfo in searchOrder:
+            searchIndex = 0
+            currentFileInfoIndex = fileSystem.index(fileInfo)
+            while searchIndex < currentFileInfoIndex:
+                searchFileInfo = fileSystem[searchIndex]
+                if searchFileInfo.freeSpace >= fileInfo.size:
+                    
+                    fileInfoIndex = fileSystem.index(fileInfo)
 
+                    # move the free space and block size of the current fileInfo
+                    # to the block preceeding its current spot in the file system
+                    prevFileIndex = fileInfoIndex - 1
+                    fileSystem[prevFileIndex].freeSpace += fileInfo.size + fileInfo.freeSpace
+                    
+                    # then move the free space to the end of the current block
+                    fileInfo.freeSpace = searchFileInfo.freeSpace - fileInfo.size
+                    # and set the free space of the test block to 0
+                    searchFileInfo.freeSpace = 0
 
-        # then expand out the harddrive so we can generate the checksum
-        harddrive = []
-        for fileInfo in self.processed:
-            harddrive.extend([fileInfo.id] * fileInfo.size)
-            harddrive.extend([-1] * fileInfo.freeSpace)
+                    # pull the fileInfo out of the file system
+                    fileSystem.pop(fileInfoIndex)
 
-        result = 0
-        for index, blockID in enumerate(harddrive):
-            result += (index * blockID)
+                    # so we can put it back after the current test block
+                    fileSystem.insert(searchIndex + 1, fileInfo)
 
-        return result
+                    # and push the searchIndex beyond the limits to break out of the loop
+                    searchIndex = len(fileSystem)
+
+                searchIndex += 1
+
+        return fileSystem.checksum()
 
 
 if __name__ == '__main__':
